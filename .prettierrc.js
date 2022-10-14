49b2c29d3b41e46e94f6185b327d902862046b01
lbbjz <lbbjz@gmail.com>
2022-10-14 20:28:13 +0800

    代码风格配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,7 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  singleQuote: true,//单引号代替双引号
+  trailingComma: 'all',//逗号
+  //semi: false,//去掉结尾的分号
 };
