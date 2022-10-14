15bc7efd332fe67c3265d8b9757c13eb1b02782c
lbbjz <lbbjz@gmail.com>
2022-10-14 23:18:57 +0800

    hooks

---
+++ .history/.prettierrc_20221014201815.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,//单引号代替双引号
+  trailingComma: 'all',//逗号
+  semi: false,//去掉结尾的分号
+};
